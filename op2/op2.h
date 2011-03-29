24e810e8cfb3d57090ffa465fb1c5b2f73e7e05b
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-03-29 18:57:56 +0100

    add global OP2 header op2.h

---
+++ op2/op2.h
@@ -0,0 +1,10 @@
+#ifndef __OP2_HEADERS
+#define __OP2_HEADERS
+
+#include <op2/common/op_datatypes.h>
+#include <op2/common/op_globals.h>
+#include <op2/common/op_common.h>
+#include <op2/common/op_par_loop.h>
+
+#endif
+
