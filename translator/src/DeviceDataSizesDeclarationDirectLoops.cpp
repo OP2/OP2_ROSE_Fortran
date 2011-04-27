d923671f0ddddd4f6aa7484eda49c441295e7050
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2011-04-27 16:32:16 +0100

    Added support for direct loops device variables sizes. Remember that direct loops only need the size of device variables corresponding to op_par_loop op_dat variables, and not also plan-related variables as in the case of indirect loops. This simply because direct loops have no plan at all.

---
+++ translator/src/DeviceDataSizesDeclarationDirectLoops.cpp
@@ -0,0 +1,120 @@
+#include <boost/lexical_cast.hpp>
+#include <DeviceDataSizesDeclarationDirectLoops.h>
+#include <FortranStatementsAndExpressionsBuilder.h>
+#include <FortranTypesBuilder.h>
+#include <OP2CommonDefinitions.h>
+
+/*
+ * ======================================================
+ * Private functions
+ * ======================================================
+ */
+void
+DeviceDataSizesDeclarationDirectLoops::addFields ( ParallelLoop & parallelLoop,
+																									 SgScopeStatement * moduleScope )
+{
+  using boost::lexical_cast;
+  using SageBuilder::buildVariableDeclaration;
+  using std::map;
+  using std::string;
+  using std::vector;
+	
+  for (unsigned int i = 1; i
+			 <= parallelLoop.getNumberOf_OP_DAT_ArgumentGroups (); ++i)
+  {
+    if (parallelLoop.isDuplicate_OP_DAT (i) == false)
+    {
+      string const variableName = "parg" + lexical_cast <string> (i)
+			+ "DatDSize";
+			
+      SgVariableDeclaration * fieldDeclaration = buildVariableDeclaration (
+																																					 variableName, FortranTypesBuilder::getFourByteInteger (), NULL,
+																																					 moduleScope);
+			
+      fieldDeclaration->get_declarationModifier ().get_accessModifier ().setUndefined ();
+			
+      deviceDatatypeStatement->get_definition ()->append_member (
+																																 fieldDeclaration);
+			
+      OP_DAT_Sizes[i] = fieldDeclaration;
+    }
+  }
+	
+//  for (unsigned int i = 1; i
+//			 <= parallelLoop.getNumberOf_OP_DAT_ArgumentGroups (); ++i)
+//  {
+//    if (parallelLoop.isDuplicate_OP_DAT (i) == false
+//        && parallelLoop.get_OP_MAP_Value (i) == INDIRECT)
+//    {
+//      string const variableName = "pindMaps" + lexical_cast <string> (i)
+//			+ "Size";
+//			
+//      SgVariableDeclaration * fieldDeclaration = buildVariableDeclaration (
+//																																					 variableName, FortranTypesBuilder::getFourByteInteger (), NULL,
+//																																					 moduleScope);
+//			
+//      fieldDeclaration->get_declarationModifier ().get_accessModifier ().setUndefined ();
+//			
+//      deviceDatatypeStatement->get_definition ()->append_member (
+//																																 fieldDeclaration);
+//			
+//      localToGlobalRenumberingOfIndirectMappingSizes[i] = fieldDeclaration;
+//    }
+//  }
+	
+//  for (unsigned int i = 1; i
+//			 <= parallelLoop.getNumberOf_OP_DAT_ArgumentGroups (); ++i)
+//  {
+//    if (parallelLoop.get_OP_MAP_Value (i) == INDIRECT)
+//    {
+//      string const variableName = "pmaps" + lexical_cast <string> (i) + "Size";
+//			
+//      SgVariableDeclaration * fieldDeclaration = buildVariableDeclaration (
+//																																					 variableName, FortranTypesBuilder::getFourByteInteger (), NULL,
+//																																					 moduleScope);
+//			
+//      fieldDeclaration->get_declarationModifier ().get_accessModifier ().setUndefined ();
+//			
+//      deviceDatatypeStatement->get_definition ()->append_member (
+//																																 fieldDeclaration);
+//			
+//      globalToLocalRenumberingOfIndirectMappingSizes[i] = fieldDeclaration;
+//    }
+//  }
+	
+//  vector <string> planFunctionSizeVariables;
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pblkMapSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pindMapsSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pindOffsSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pindSizesSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pnelemsSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pnthrcolSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::poffsetSize);
+//  planFunctionSizeVariables.push_back (PlanFunctionSizeVariables::pthrcolSize);
+//	
+//  for (vector <string>::iterator it = planFunctionSizeVariables.begin (); it
+//			 != planFunctionSizeVariables.end (); ++it)
+//  {
+//    SgVariableDeclaration * fieldDeclaration = buildVariableDeclaration (*it,
+//																																				 FortranTypesBuilder::getFourByteInteger (), NULL, moduleScope);
+//		
+//    fieldDeclaration->get_declarationModifier ().get_accessModifier ().setUndefined ();
+//		
+//    deviceDatatypeStatement->get_definition ()->append_member (fieldDeclaration);
+//		
+//    otherFieldDeclarations[*it] = fieldDeclaration;
+//  }
+}
+
+/*
+ * ======================================================
+ * Public functions
+ * ======================================================
+ */
+
+DeviceDataSizesDeclarationDirectLoops::DeviceDataSizesDeclarationDirectLoops ( ParallelLoop & parallelLoop, 
+  std::string const & subroutineName, SgScopeStatement * moduleScope
+	): DeviceDataSizesDeclaration ( parallelLoop, subroutineName, moduleScope )
+{
+	
+}
