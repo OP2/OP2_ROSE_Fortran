f8fc08f313c2e767ffeffe94c826b615b9a187bc
Dr. Carlo Bertolli <c.bertolli@imperial.ac.uk>
2012-01-06 13:26:29 +0000

    The compiler for Fortran to CUDA now eliminates "use" statements inside user kernels to avoid resolution of called subroutines outside the scope of the generated cuda module

--- translator/src/Fortran/CUDA/Common/FortranCUDAUserSubroutine.cpp
+++ translator/src/Fortran/CUDA/Common/FortranCUDAUserSubroutine.cpp
@@ -10,91 +10,6 @@
 #include <boost/algorithm/string/predicate.hpp>
 #include <algorithm>
 
-// void 
-// FortranCUDAUserSubroutine::patchCalledSubroutinesNames (string oldSubroutineName,
-//   SgProcedureHeaderStatement * calledSubroutineReference)
-// {
-//   using namespace SageInterface;
-//   using boost::iequals;
-//   using std::string;
-//   using std::vector;
-  
-//   class TreeVisitor: public AstSimpleProcessing
-//   {
-//   private:
-//     /*
-//      * ======================================================
-//      * The recursive visit of a user subroutine changes
-//      * the references to the user subroutines to the passed
-//      * one as formal parameter to this function
-//      * ======================================================
-//      */            
-//     vector < SgProcedureHeaderStatement * > calledRoutines;
-    
-//   public:
-    
-//     vector < SgProcedureHeaderStatement * > getCalledRoutinesInStatement()
-//     {
-//       return calledRoutines;
-//     }
-    
-//     TreeVisitor ()
-//     {
-//     }
-    
-//     virtual void
-//     visit (SgNode * node)
-//     {
-//       SgExprStatement * isExprStatement = isSgExprStatement ( node );
-//       if ( isExprStatement != NULL )
-//       {      
-//         SgFunctionCallExp * functionCallExp = isSgFunctionCallExp ( isExprStatement->get_expression() );
-        
-//         if ( functionCallExp != NULL )
-//         {
-//           string const
-//           calleeName =
-//           functionCallExp->getAssociatedFunctionSymbol ()->get_name ().getString ();
-          
-//           Debug::getInstance ()->debugMessage ("Found function call in user subroutine "
-//                                                + calleeName + "'", Debug::OUTER_LOOP_LEVEL, __FILE__, __LINE__);
-          
-//           if ( iequals (calleeName, oldSubroutineName)
-//           {
-//             /*
-//              * ======================================================
-//              * Patching the name of the called function
-//              * ======================================================
-//              */
-//             SgFunctionRefExp * referenceToNewName = buildFunctionRefExp (
-//               calledSubroutineReference);
-
-//             functionCallExp->set_function (referenceToNewName);
-//           }
-//         }
-//       }
-//     }
-//   };
-  
-//   vector <SgStatement *> originalStatements =
-//   originalSubroutine->get_definition ()->get_body ()->get_statements ();
-  
-//   for (vector <SgStatement *>::iterator it = originalStatements.begin (); it
-//        != originalStatements.end (); ++it)
-//   {      
-//     /*
-//      * ======================================================
-//      * Recursively look for subroutine calls inside shallow
-//      * nodes in the routines to change the name to the 
-//      * modified name
-//      * ======================================================
-//      */                  
-//     TreeVisitor * visitor = new TreeVisitor ();
-    
-//     visitor->traverse (*it, preorder);
-//   }
-// }
-
 void
 FortranCUDAUserSubroutine::createStatements ()
 {
@@ -207,12 +122,26 @@ FortranCUDAUserSubroutine::createStatements ()
 
     if (isVariableDeclaration == NULL)
     { 
-      Debug::getInstance ()->debugMessage (
+      /*
+       * ======================================================
+       * Do not append use statement, because other subroutines
+       * are directly appended to the CUDA module
+       * ======================================================
+       */                  
+      SgUseStatement * isUseStmt = isSgUseStatement ( *it );
+      if (isUseStmt != NULL)
+	{
+	  Debug::getInstance ()->debugMessage (
+          "Not appending use statement",
+          Debug::HIGHEST_DEBUG_LEVEL, __FILE__, __LINE__);
+	}
+      else
+	{
+	  Debug::getInstance ()->debugMessage (
           "Appending (non-variable-declaration) statement",
           Debug::HIGHEST_DEBUG_LEVEL, __FILE__, __LINE__);
 
-      appendStatement (*it, subroutineScope);
-
+	  appendStatement (*it, subroutineScope);	
       /*
        * ======================================================
        * Recursively look for subroutine calls inside shallow
@@ -221,20 +150,20 @@ FortranCUDAUserSubroutine::createStatements ()
        * and append it to the userSubroutine vector
        * ======================================================
        */                  
-      TreeVisitor * visitor = new TreeVisitor ();
-
-      visitor->traverse (*it, preorder);
-
-      Debug::getInstance ()->debugMessage ("Appending deep subroutine calls", Debug::OUTER_LOOP_LEVEL, __FILE__, __LINE__);
+	  TreeVisitor * visitor = new TreeVisitor ();
+	  
+	  visitor->traverse (*it, preorder);
+      
+	  Debug::getInstance ()->debugMessage ("Appending deep subroutine calls", Debug::OUTER_LOOP_LEVEL, __FILE__, __LINE__);
 
       
-      vector < SgProcedureHeaderStatement * > deepStatementCalls = visitor->getCalledRoutinesInStatement ();
-      vector < SgProcedureHeaderStatement * >::iterator itDeepCalls;
-      for (itDeepCalls = deepStatementCalls.begin(); itDeepCalls != deepStatementCalls.end(); ++itDeepCalls)
-        calledRoutines.push_back (*itDeepCalls);
+	  vector < SgProcedureHeaderStatement * > deepStatementCalls = visitor->getCalledRoutinesInStatement ();
+	  vector < SgProcedureHeaderStatement * >::iterator itDeepCalls;
+	  for (itDeepCalls = deepStatementCalls.begin(); itDeepCalls != deepStatementCalls.end(); ++itDeepCalls)
+	    calledRoutines.push_back (*itDeepCalls);
       
-       Debug::getInstance ()->debugMessage ("Appending deep subroutine calls", Debug::OUTER_LOOP_LEVEL, __FILE__, __LINE__);
-      
+	  Debug::getInstance ()->debugMessage ("Appending deep subroutine calls", Debug::OUTER_LOOP_LEVEL, __FILE__, __LINE__);
+	}
     }
     else
     {
@@ -260,8 +189,6 @@ FortranCUDAUserSubroutine::createStatements ()
          * ======================================================
          */                  
         
-	//        if ( isUserKernel == true )
-        //{
 	bool isFormalParamater = false;
 	
 	for (SgInitializedNamePtrList::iterator paramIt =
@@ -353,17 +280,6 @@ FortranCUDAUserSubroutine::createStatements ()
                   FortranStatementsAndExpressionsBuilder::appendVariableDeclaration (
                       variableName, type, subroutineScope);
 	    }
-	  //        } else {
-	  //          Debug::getInstance ()->debugMessage ("'" + variableName
-	  //            + "' is a formal parameter", Debug::HIGHEST_DEBUG_LEVEL,
-	  //            __FILE__, __LINE__);
-          
-	  //                SgVariableDeclaration
-	  //                    * variableDeclaration =
-	  //                        FortranStatementsAndExpressionsBuilder::appendVariableDeclarationAsFormalParameter (
-	  //                            variableName, type, subroutineScope,
-	  //                            formalParameters, 0);
-	  //}
       }
     }
   }
@@ -402,17 +318,8 @@ void FortranCUDAUserSubroutine::appendAdditionalSubroutines ( SgScopeStatement *
   vector < SgProcedureHeaderStatement * > :: iterator it;
   for ( it = calledRoutines.begin(); it != calledRoutines.end(); it++ )
   {
-   /*
-    * ======================================================
-    * Each subroutine takes its original name followed by
-    * "_" and the name of the caller, to avoid to manage
-    * consistency between same routine called in different
-    * positions (i.e. for the "value" attribute)
-    * ======================================================
-    */
    
     string calledSubroutineName = (*it)->get_name ().getString ();
-    // + "_" + subroutineName;
     
     Debug::getInstance ()->debugMessage ("Appending new subroutine '"
         + calledSubroutineName + "'", Debug::FUNCTION_LEVEL, __FILE__, __LINE__);
@@ -420,17 +327,7 @@ void FortranCUDAUserSubroutine::appendAdditionalSubroutines ( SgScopeStatement *
     FortranCUDAUserSubroutine * newRoutine = new FortranCUDAUserSubroutine ( moduleScope, 
         parallelLoop, declarations, calledSubroutineName );
    
-    additionalSubroutines.push_back (newRoutine);
-    
-    /*
-     * ======================================================
-     * As the name of the called routine has changed, I need
-     * to patch all references in the caller
-     * ======================================================
-     */
-//    patchCalledSubroutinesNames ((*it)->get_name ().getString (),
-//      newRoutine->getSubroutineHeaderStatement());
-    
+    additionalSubroutines.push_back (newRoutine);        
   }
   
   vector < FortranUserSubroutine * > :: iterator itRecursive;
